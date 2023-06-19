25ee22db7783628a454b8e22d7e1a0fe67aaea53
mamalmaleki <mohammadalimalekie@gmail.com>
2023-06-19 17:28:36 +0330

    add gorm orm to project

--- internal/service/publisher.go
+++ internal/service/publisher.go
@@ -1,6 +1,11 @@
 package service
 
+import (
+	"gorm.io/gorm"
+)
+
 type Publisher struct {
-	newPostReader *kafka.Reader
-	publishedPostWriter
+	newPostReader       *kafka.Reader
+	publishedPostWriter *kafka.Writer
+	db                  *gorm.DB
 }
