88099132b1c0ad18a4dd4fc564a21184867122bf
mamalmaleki <mohammadalimalekie@gmail.com>
2023-06-19 17:58:07 +0330

    add gorm postgres driver to project

--- go.mod
+++ go.mod
@@ -4,12 +4,18 @@ go 1.20
 
 require (
 	github.com/segmentio/kafka-go v0.4.40
+	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.1
 )
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
--- internal/service/publisher.go
+++ internal/service/publisher.go
@@ -1,7 +1,8 @@
 package service
 
 import (
-	kafka "github.com/segmentio/kafka-go"
+	"github.com/segmentio/kafka-go"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -9,4 +10,21 @@ type Publisher struct {
 	newPostReader       *kafka.Reader
 	publishedPostWriter *kafka.Writer
 	db                  *gorm.DB
+}
+
+func NewPublisher() (*Publisher, func()) {
+
+	p := &Publisher{}
+
+	//mechanism, err := scram.Mechanism(scram.SHA256, "","")
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	// setup database
+	dsn := "postgres://pg:pass@localhost:5432/crud"
+	//dsn := "host=localhost user= password= dbname=posts sslmode=disable TimeZone=Asia/Tehran"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	return &Publisher{}, nil
 }
